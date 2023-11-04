84c5e3e236d2994f908049ccbec4a348e70f0aed
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2023-11-04 16:01:03 -0700

    working through the tutorial
    https://www.youtube.com/watch?app=desktop&v=0YFrGy_mzjY&ab_channel=GreatStack

---
+++ index.js
@@ -0,0 +1,13 @@
+var tabLinks = document.getElementsByClassName('tab-links')
+var tabContents = document.getElementsByClassName('tab-contents')
+
+function opentab(tabName, el) {
+    for (var tabLink of tabLinks) {
+        tabLink.classList.remove('active-link');
+    }
+    for (var tabContent of tabContents) {
+        tabContent.classList.remove('active-tab');
+    }
+    el.classList.add('active-link');
+    document.getElementById(tabName).classList.add('active-tab');
+}
